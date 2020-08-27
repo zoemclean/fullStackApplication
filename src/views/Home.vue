2f68d50b2d67532cbbe2cd5092d171799fe8999a
Ben Elwood <ben.elwoods@gmail.com>
2020-08-28 11:07:10 +1200

    added post functionaly

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,11 +6,10 @@
     <div class="bg-light border-right" id="sidebar-wrapper">
       <div class="list-group list-group-flush">
         <a href="#" class="list-group-item list-group-item-action bg-light">Business</a>
-        <a href="#" class="list-group-item list-group-item-action bg-light">Nature / landscapes</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">Nature / Landscapes</a>
         <a href="#" class="list-group-item list-group-item-action bg-light">Sea Life</a>
         <a href="#" class="list-group-item list-group-item-action bg-light">House Pets</a>
         <a href="#" class="list-group-item list-group-item-action bg-light">Architecture</a>
-        <a href="#" class="list-group-item list-group-item-action bg-light">Technology</a>
         <a href="#" class="list-group-item list-group-item-action bg-light">Minimalism</a>
         <a href="#" class="list-group-item list-group-item-action bg-light">Photos from film rolls</a>
       </div>
@@ -18,52 +17,23 @@
     <!-- sidebar END -->
 
     <!-- Page Content -->
-    <div id="page-content-wrapper">
-      <div class="container-fluid">
+    <div id="page-content-wrapper" v-if="items">
+      <div class="container-fluid" v-for="item in items" :key="item.title">
         <div class="row">
           <div class="col-12 mt-3">
             <div class="card">
                 <div class="card-horizontal">
-                    <div class="img-square-wrapper">
-                        <img class="" src="http://via.placeholder.com/600x300" alt="Card image cap">
+                    <div class="img-square-wrapper" v-bind:style="{background: 'url(' + item.postImage + ') no-repeat center center'}" style="background-size: cover;">
+                        <!-- <img src="http://via.placeholder.com/600x300" alt="Card image cap"> -->
                     </div>
                     <div class="card-body content">
-                        <h4 class="card-title">Card title</h4>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        </div>
-      </div>
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-12 mt-3">
-            <div class="card">
-                <div class="card-horizontal">
-                    <div class="img-square-wrapper">
-                        <img class="" src="http://via.placeholder.com/600x300" alt="Card image cap">
-                    </div>
-                    <div class="card-body content">
-                        <h4 class="card-title">Card title</h4>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        </div>
-      </div>
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-12 mt-3">
-            <div class="card">
-                <div class="card-horizontal">
-                    <div class="img-square-wrapper">
-                        <img class="" src="http://via.placeholder.com/600x300" alt="Card image cap">
-                    </div>
-                    <div class="card-body content">
-                        <h4 class="card-title">Card title</h4>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <div class="category">
+                          <p>{{item.postCategory}}</p>
+                          <h5 class="card-title">{{item.postUser}}</h5>
+                        </div>
+                        <h3 class="card-title">{{item.postTitle}}</h3>
+                        <p class="card-text">{{item.postContent}}</p>
+                        <h4 class="date">{{item.postDate}}</h4>
                     </div>
                 </div>
             </div>
@@ -78,11 +48,25 @@
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
+import firebase from '../firebase.js'
 
 export default {
   name: 'Home',
-  components: {
-    // HelloWorld
+  data () {
+    return {
+      items: false
+    }
+  },
+  created () {
+    var data = []
+    const v = this
+    firebase.collection('posts').onSnapshot(function (items) {
+      items.forEach(function (doc) {
+        var eachDoc = doc.data()
+        data.push(eachDoc)
+      })
+      v.items = data
+    })
   }
 }
 </script>
@@ -109,5 +93,23 @@ export default {
 
 .cardWidth {
     width: 22rem;
+}
+div.img-square-wrapper {
+  padding: 10% 20%;
+  background-size: cover;
+}
+.category {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  h5 {
+    margin-left: 10px;
+  }
+  & p {
+    text-transform: capitalize;
+    background-color: black;
+    color: white;
+    padding: 2px 5px;
+  }
 }
 </style>
--- src/views/NewPost.vue
+++ src/views/NewPost.vue
@@ -1,36 +1,123 @@
 <template lang="html">
   <div class="account-cont marginTop container">
-    <div class="col col-6">
+    <div class="col col-6" v-if="signedIn">
       <h2>Create New Post</h2>
-    <form>
+      <p v-if="successIsActive">Successfully Posted!</p>
+      <p v-if="errorIsActive">{{errorIsActive}}</p>
+    <div>
+      <div class="form-row">
+        <div class="form-group col-md-12">
+          <label for="content-title">Title</label>
+          <input type="text" class="form-control" id="contentTitle" placeholder="Enter Content Title" v-model="postTitle">
+        </div>
+      </div>
       <div class="form-row">
         <div class="form-group col-md-6">
-          <label for="content-title">Title</label>
-          <input type="text" class="form-control" id="contentTitle" placeholder="Enter Content Title">
+          <label for="content-tags">Tags</label>
+          <input type="text" class="form-control" id="contentTags" placeholder="#Tags" v-model="postTags">
         </div>
         <div class="form-group col-md-6">
-          <label for="content-tags">Tags</label>
-          <input type="text" class="form-control" id="contentTags" placeholder="#Tags">
+          <label for="content-title">Category</label>
+          <select type="text" class="form-control" id="contentTitle" v-model="postCategory">
+            <option value="business">Business</option>
+            <option value="natureLandscapes">Nature / Landscapes</option>
+            <option value="seaLife">Sea Life</option>
+            <option value="housePets">House Pets</option>
+            <option value="architecture">Architecture</option>
+            <option value="minimalism">Minimalism</option>
+            <option value="photosFromFilmRolls">Photos from Film Rolls</option>
+          </select>
         </div>
       </div>
       <div class="form-group">
         <label for="content-description">Description</label>
-        <textarea class="form-control" id="contentDescription" rows="3"></textarea>
+        <textarea class="form-control" id="contentDescription" rows="3" v-model="postContent"></textarea>
       </div>
       <!-- W3 Schools custom file uploader -->
       <label for="content-file">Upload Image</label>
       <div class="custom-file mb-3">
-      <input type="file" class="custom-file-input" id="customFile" name="filename">
-      <label class="custom-file-label" for="customFile">Choose file</label>
+      <!-- <input type="file" class="custom-file-input" id="customFile" name="filename"> -->
+      <input name="customFile" class="custom-file-input" id="inp" @change="previewFiles" type='file'>
+      <label class="custom-file-label" for="inp">Choose file</label>
     </div>
-  <button type="submit" class="btn btn-primary">Post</button>
-</form>
+  <button type="submit" class="btn btn-primary" @click="uploadPost">Post</button>
+</div>
+  </div>
+  <div v-else>
+    <h3>You must <router-link to="/Login">log in</router-link> or <router-link to="/Login">sign up</router-link> to view this page.</h3>
   </div>
 </div>
 </template>
 
 <script>
+import database from '../firebase.js'
+import moment from 'moment'
+
 export default {
+  data () {
+    return {
+      postUser: '',
+      postUserEmail: '',
+      postTags: '',
+      postCategory: '',
+      postContent: '',
+      postTitle: '',
+      postImage: '',
+      signedIn: false,
+      errorIsActive: '',
+      successIsActive: false
+    }
+  },
+  created () {
+    const signedIn = localStorage.getItem('signedIn')
+    if (signedIn === 'true') {
+      this.signedIn = true
+      this.postUserEmail = localStorage.getItem('userEmail')
+      const v = this
+      database.collection('users').doc(this.postUserEmail).get().then(function (doc) {
+        v.postUser = doc.data().userName
+      })
+    } else {
+      this.signedIn = false
+    }
+  },
+  methods: {
+    previewFiles (event) {
+      var v = this
+      const toBase64 = file => new Promise((resolve, reject) => {
+        const reader = new FileReader()
+        reader.readAsDataURL(file)
+        reader.onload = () => resolve(reader.result)
+        reader.onerror = error => reject(error)
+      })
+
+      async function Main () {
+        v.postImage = await toBase64(event.target.files[0])
+        console.log('done')
+      }
+
+      Main()
+    },
+    uploadPost: function () {
+      const date = moment().format('MMMM Do YYYY, h:mm:ss a')
+      const v = this
+      database.collection('posts').doc().set({
+        postUser: v.postUser,
+        postImage: v.postImage,
+        postTitle: v.postTitle,
+        postContent: v.postContent,
+        postCategory: v.postCategory,
+        postTags: v.postTags,
+        postDate: date
+      }).then(function () {
+        console.log('Document successfully written!')
+        v.successIsActive = true
+      }).catch(function (error) {
+        console.error('Error writing document: ', error)
+        v.errorIsActive = error.message
+      })
+    }
+  }
 }
 </script>
 
